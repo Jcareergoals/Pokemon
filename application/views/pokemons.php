979dd0c67f163abda0ead592904cc8f64944bb69
Jose Chery <Jcareergoals@hotmail.com>
2016-07-17 01:18:23 -0400

    Refactored. Added ID's to select 'div' elements & added 'php' to php open tags --> <?php ...?>

--- application/views/pokemons.php
+++ application/views/pokemons.php
@@ -22,9 +22,8 @@
 <body>
 	<div class="container-fluid col-md-10 col-md-offset-1">
 		<div class="row">
-			<div class="header col-sm-10 col-xs-offset-1">
-				<h1>Welcome to Pokemons</h1>
-				<p>Click on a pokemon to see it's information in the pokedex</p>
+			<div id="header" class="col-sm-10 col-xs-offset-1">
+				<h1>Welcome to PKMons!</h1>
 			</div>
 		</div>
 		<div class="row">
@@ -32,18 +31,22 @@
 			<div class="col-xs-12 col-sm-9 pokemons">
 				<?php for($x = 1; $x < 156; $x++) 
 				{?>
-					<img class='pokemon' id='<? echo $x; ?>' src="http://pokeapi.co/media/img/<? echo $x;?>.png" width='100px' height='100px'>
+					<img class='pokemon' id='<?php echo $x; ?>' src="http://pokeapi.co/media/img/<?php echo $x;?>.png" width='100px' height='100px'>
 				<?php } ?>
 			</div>
 			<!-- POKEDEX SECTION -->
 			<div class="col-xs-12 col-sm-3">
-				<div class="pokedex col-xs-3">
+				<div id="pokedex" class="col-xs-3">
+					<h1 class='pokedex_heading'>POKEDEX</h1>
+					<div id="pokedex_content">
+						<h5>Select a pokemon character to view it's information.</h5>
+					</div>
 				</div>
 			</div>
 		</div>
 		<!-- COPYRIGHT SECTION -->
 		<div class="row">
-			<div class="col-xs-12">
+			<div id='copyright' class="col-xs-12">
 				<span class="copyright">Copyright &copy; 2016 by <a href="https://www.linkedin.com/in/jose-chery-763110a1" target="_blank">Jose Chery</a></span>
 			</div>
 		</div>
